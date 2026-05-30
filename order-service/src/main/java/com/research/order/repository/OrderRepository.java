2a566052c6237b517c03b8c5d4c16779f7c11fb3
yasithwimukthi <wimukthiyasith@gmail.com>
2026-05-30 13:26:21 +0530

    implement order service

---
+++ order-service/src/main/java/com/research/order/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.research.order.repository;
+
+import com.research.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCustomerId(String customerId);
+    List<Order> findByStatus(Order.OrderStatus status);
+}
